685be007c2ad71a40b693063165a42cbaa8b00ef
Chennakesava-N220287 <n220287@rguktn.ac.in>
2026-02-27 23:43:28 +0530

    Initial registration system with validation

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>User Registration</title>
+</head>
+<body>
+    <h2>Register</h2>
+    <form method="POST" action="register.php">
+        Name: <input type="text" name="name"><br><br>
+        Email: <input type="text" name="email"><br><br>
+        Password: <input type="password" name="password"><br><br>
+        <input type="submit" value="Register">
+    </form>
+</body>
+</html>
---
+++ register.php
@@ -0,0 +1,23 @@
+<?php
+
+function validate_user($name, $email, $password) {
+    if (empty($name)) {
+        return "Name is required";
+    }
+
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        return "Invalid email format";
+    }
+
+    if (strlen($password) < 6) {
+        return "Password must be at least 6 characters";
+    }
+
+    return "Success";
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $result = validate_user($_POST["name"], $_POST["email"], $_POST["password"]);
+    echo $result;
+}
+?>
