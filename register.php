bcacfc697bffd248e9633891ac897d41774bcff8
Chennakesava-N220287 <n220287@rguktn.ac.in>
2026-03-01 16:09:59 +0530

    solved bug second time

--- register.php
+++ register.php
@@ -9,7 +9,7 @@ function validate_user($name, $email, $password) {
         return "Invalid email format";
     }
 
-    if (strlen($password) < 19) {
+    if (strlen($password) < 6) {
         return "Password must be at least 6 characters";
     }
 
